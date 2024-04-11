df0fc77198a6ad239f04d86af3b84522c91b7781
IjonK007 <kushta.joni@gmail.com>
2024-04-11 13:44:12 +0200

    update font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { cn } from "../lib/utils";
 
 import FloatingNav from "../components/FloatingNav";
 import Footer from "../components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Ijon Kushta",
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(inter.className, "flex flex-col bg-black")}>
+      <body className={cn(montserrat.className, "flex flex-col bg-black")}>
         <FloatingNav
           navItems={[
             { name: "Home", link: "hero" },
