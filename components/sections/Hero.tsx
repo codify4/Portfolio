338390d304e9e8022ce93610a36870a80e38aa43
IjonK007 <kushta.joni@gmail.com>
2024-04-12 17:18:02 +0200

    About Me section finished

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,15 +1,14 @@
 "use client"
-
-import { HeroHighlight, Highlight } from "./DotBg"
-import Button from "./Button"
+import { HeroHighlight, Highlight } from "../DotBg"
+import Button from "../Button"
 
 const Hero = () => {
-
+  
   //TODO: TAKE A PIC AND PUT IT HERE, NO BG
 
   return (
     <div id="hero" className="bg-black text-white">
-        <HeroHighlight className="flex flex-row items-center justify-center mx-5 text-6xl gap-[600px]">
+        <HeroHighlight className="flex flex-row items-center justify-center text-6xl gap-[600px]">
           <div className="flex flex-col items-start gap-y-3">
             <span className="font-bold">Hello,</span>
             <span className="font-bold"> I am Ijon Kushta!!! </span>
