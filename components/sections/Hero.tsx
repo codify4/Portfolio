68b739db3b5d64a5715ec18599af8e5b86923cc7
IjonK007 <kushta.joni@gmail.com>
2024-04-17 18:20:53 +0200

    hero section responsive attempt 1

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -8,11 +8,11 @@ const Hero = () => {
 
   return (
     <div id="hero" className="bg-black text-white">
-        <HeroHighlight className="flex flex-row items-center justify-center text-6xl gap-[600px]">
-          <div className="flex flex-col items-start gap-y-3">
+        <HeroHighlight className="flex flex-row items-center justify-center text-6xl gap-[600px] xl:px-[100px] lg:px-[200px]">
+          <div className="flex flex-col flex-wrap w-full items-start gap-y-3">
             <span className="font-bold">Hello,</span>
             <span className="font-bold"> I am Ijon Kushta!!! </span>
-            <Highlight className="text-6xl rounded-[20px] font-bold">Front-End Developer!</Highlight>
+            <Highlight className="xl:w-[700px] md:w-[400px]text-6xl rounded-[20px] font-bold ">Front-End Developer!</Highlight>
             
             <a href={`mailto:${process.env.EMAIL}`} target="_blank" >
               <Button />
