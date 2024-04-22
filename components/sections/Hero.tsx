56fb3142db873424d9c032dcb535769484a5dca8
IjonK007 <kushta.joni@gmail.com>
2024-04-22 18:01:05 +0200

    little width change on hero

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -9,10 +9,10 @@ const Hero = () => {
   return (
     <div id="hero" className="bg-black text-white">
         <HeroHighlight className="flex flex-row items-center justify-center text-6xl 2xl:gap-x-[500px] xl:gap-x-[100px] md:gap-x-[100px] xl:px-[100px] lg:px-[200px]">
-          <div className="flex flex-col w-full items-start gap-y-2">
+          <div className="flex flex-col xl:w-full md:w-[600px] items-start gap-y-2">
             <span className="font-bold">Hello,</span>
-            <span className="font-bold w-[700px]"> I am Ijon Kushta!!! </span>
-            <Highlight className="xl:w-[700px] text-6xl rounded-[20px] font-bold ">Front-End Developer!</Highlight>
+            <span className="font-bold w-[600px]"> I am Ijon Kushta </span>
+            <Highlight className="xl:w-[700px] md:w-[360px] text-6xl rounded-[20px] font-bold ">Front-End Developer</Highlight>
             
             <a href={`mailto:${process.env.EMAIL}`} target="_blank" >
               <Button />
