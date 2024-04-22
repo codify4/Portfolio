1648245d7ac6bdd2fb6f838165490c3f3160c07c
IjonK007 <kushta.joni@gmail.com>
2024-04-22 20:33:10 +0200

    project section paralax removed

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -1,24 +1,19 @@
 
-import { cn, projects } from '@/lib/utils';
+import { projects } from '@/lib/utils';
 import { ParallaxScrollSecond } from "../parallax-scroll";
 import FadeInOnScroll  from "../FadeIn";
 
 const Projects = () => {
-  const width = 1
 
   return (
     <FadeInOnScroll duration={0.5}>
       <div id='projects' className='text-white xl:w-[1200px] md:w-[800px] w-[400px] mt-14'>
         <h1 className='text-6xl font-bold mb-10'>Projects</h1>
-        { width === 680 ? (
-          <ParallaxScrollSecond images={projects} />
-        ) : (
-          <div>
+          <div className='grid xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'>
             {projects.map((image, index) => (
               <img src={image.src} alt={image.alt} key={index} className='mt-2 ml-[40px] w-[300px]' />
             ))}
           </div>
-        )}
       </div>
     </FadeInOnScroll>
   )
