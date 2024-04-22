66cce4a8293af97c207278d5145f676a8abc167c
IjonK007 <kushta.joni@gmail.com>
2024-04-22 19:57:19 +0200

    window not defined on projects section

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -3,24 +3,19 @@
 import { projects } from '@/lib/utils';
 import { ParallaxScrollSecond } from "../parallax-scroll";
 import FadeInOnScroll  from "../FadeIn";
-import { useEffect, useState } from 'react';
+import useWindowSize from "@rooks/use-window-size"
+
 
 const Projects = () => {
 
-  const [width, setWidth] = useState(window.innerWidth);
-  
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+  const { innerWidth, innerHeight, outerHeight, outerWidth } = useWindowSize();
   
 
   return (
     <FadeInOnScroll duration={0.5}>
       <div id='projects' className='text-white xl:w-[1200px] md:w-[800px] w-[400px] mt-14'>
         <h1 className='text-6xl font-bold mb-10'>Projects</h1>
-        {window.innerWidth > 680 ? (
+        {innerWidth > 680 ? (
           <ParallaxScrollSecond images={projects} />
         ) : (
           <div>
